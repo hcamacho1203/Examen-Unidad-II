50be2d3851fb4112cce10470e761e0889b593253
Javierdma <141789221+Javierdma@users.noreply.github.com>
2024-05-06 22:45:33 -0700

    eliminación de comentarios

--- sketch.js
+++ sketch.js
@@ -1,21 +1,17 @@
-let horaLaPaz = 20; // Hora en La Paz, BCS
-let minutoLaPaz = 30; // Minuto en La Paz, BCS
-let horaCDMX = 0; // Hora en Ciudad de México
-let horaBarcelona = 0; // Hora en Barcelona, Esp
+let horaLaPaz = 20; 
+let minutoLaPaz = 30; 
+let horaCDMX = 0; 
+let horaBarcelona = 0; 
 let inputHora;
 let botonActualizar;
-
 function setup() {
   createCanvas(800, 400);
-  // Input para configurar hora en La Paz, BCS
   inputHora = createInput();
   inputHora.position(20, height + 20);
-  // Botón para actualizar la hora
   botonActualizar = createButton('Actualizar');
   botonActualizar.position(inputHora.x + inputHora.width, height + 20);
   botonActualizar.mousePressed(actualizarHora);
 }
-
 function draw() {
   background(220);
   // Dibujar relojes
@@ -23,11 +19,9 @@ function draw() {
   dibujarReloj(width / 2, height / 2, horaCDMX, 0, 'Ciudad de México');
   dibujarReloj(3 * width / 4, height / 2, horaBarcelona, 0, 'Barcelona, Esp');
 }
-
 function dibujarReloj(x, y, hora, minuto, ciudad) {
   // Dibujar circunferencia
   ellipse(x, y, 200, 200);
-  // Dibujar números de hora
   for (let i = 0; i < 12; i++) {
     let angulo = map(i + 1, 0, 12, 0, TWO_PI) - HALF_PI;
     let posX = x + cos(angulo) * 80;
@@ -35,7 +29,6 @@ function dibujarReloj(x, y, hora, minuto, ciudad) {
     textAlign(CENTER, CENTER);
     text(i + 1, posX, posY);
   }
-  // Calcular posiciones de las manecillas
   let segundero = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
   let minutero = map(minuto, 0, 60, 0, TWO_PI) - HALF_PI;
   let horario = map(hora % 12 + minuto / 60, 0, 12, 0, TWO_PI) - HALF_PI; // Usamos la hora configurada en lugar de la hora actual
@@ -47,26 +40,21 @@ function dibujarReloj(x, y, hora, minuto, ciudad) {
   textAlign(CENTER, TOP);
   text(ciudad, x, y + 100);
 }
-
 function dibujarManecilla(x, y, longitud, angulo) {
   let x2 = x + cos(angulo) * longitud;
   let y2 = y + sin(angulo) * longitud;
   line(x, y, x2, y2);
 }
-
 function actualizarHora() {
   let horasIngresadas = inputHora.value().split(':');
   let horas = parseInt(horasIngresadas[0]);
   let minutos = parseInt(horasIngresadas[1]);
-  // Validar que las horas y minutos estén en un rango válido
   if (horas >= 0 && horas < 24 && minutos >= 0 && minutos < 60) {
     horaLaPaz = horas;
     minutoLaPaz = minutos;
-    // Calcular diferencias de horas
-    let diferenciaHoraCDMX = 1; // Diferencia horaria con La Paz
-    // Calcular horas en otras ciudades
+    let diferenciaHoraCDMX = 1; 
     horaCDMX = (horaLaPaz + diferenciaHoraCDMX) % 24;
-    let diferenciaHoraBarcelona = 9; // Diferencia horaria con La Paz
+    let diferenciaHoraBarcelona = 9; 
     horaBarcelona = (horaLaPaz + diferenciaHoraBarcelona) % 24;
   } else {
     alert('Por favor ingrese una hora válida en formato HH:MM.');
